3bc999cd4f1bf0e3175d1e0365ff500e03af03a2
Maelhann Roze <ankou@roaming-54-ee-75-d1-cb-78.ic.ac.uk>
2018-06-24 11:54:18 -0500

    start testing Qubit

--- src/Qubit.java
+++ src/Qubit.java
@@ -2,17 +2,28 @@ import org.jblas.ComplexDoubleMatrix;
 
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Qubit {
+class Qubit {
     private static final int ALLOWABLE_QUBIT_ERROR = 1;
     private static final int RANDOM_RANGE = 1;
-    ComplexDoubleMatrix state;
+    private ComplexDoubleMatrix state;
 
     Qubit(ComplexDoubleMatrix state) {
         this.state = state;
     }
 
+    public static int getAllowableQubitError() {
+        return ALLOWABLE_QUBIT_ERROR;
+    }
 
-    boolean isValid() {
+    public static int getRandomRange() {
+        return RANDOM_RANGE;
+    }
+
+    public ComplexDoubleMatrix getState() {
+        return state;
+    }
+
+    private boolean isValid() {
         double sum = 0;
         for (Double d : state.data) {
             sum += d;
@@ -21,26 +32,45 @@ public class Qubit {
     }
 
 
-    boolean measure(int qubit) {
+    private int measure(int qubit) {
         assert isValid();
         double cursor = ThreadLocalRandom.current().nextDouble(0, RANDOM_RANGE + 1);
         for (int i = 0; i < state.length; i++) {
             cursor -= state.data[i];
             if (cursor <= 0) {
                 collapse(i);
-                return getBit(i, qubit); // what does getBit do >??/
+                getState().print();
+                return (qubit >> (i - 1)) & 1;
             }
         }
-        return false;
+        try {
+            throw new InterruptedException("invalid measurement");
+        } catch (InterruptedException e) {
+            // error catching for invalid qubit measurements
         }
+        return -1;
+    }
 
 
-    int collapse(int entry) {
+    private int collapse(int entry) {
         for (int i = 0; i < state.length; i++) {
             state.data[i] = 0;
         }
         state.data[entry] = 1;
         return entry;
+    }
+
+    private Qubit combine(Qubit q2) {
+        double[] tensorData
+                = new double[q2.state.length * this.state.length];
+        for (int i = 0; i < this.state.length; i++) {
+            for (int j = 0; j < q2.state.length; j++) {
+                tensorData[i * q2.state.length + j]
+                        = this.state.data[i] * q2.state.data[j];
+            }
+        }
+        ComplexDoubleMatrix newState = new ComplexDoubleMatrix(tensorData);
+        return new Qubit(newState);
     }
 
 
