c44d4ffe70aec36b7d77ecdf52bb22a8161bc680
Maelhann Roze <ankou@roaming-54-ee-75-d1-cb-78.ic.ac.uk>
2018-07-11 14:57:24 -0500

    parser working, now implementing the qubits for the circuit assembler

--- src/Circuit_Assembler.java
+++ src/Circuit_Assembler.java
@@ -1,21 +1,32 @@
+import org.jblas.ComplexDouble;
+
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Circuit_Assembler {
+
+
     public static void main(String[] args) {
         System.out.println("\n |--------------------------------------------------|\n" +
-                           " |   WELCOME TO M.ROZE'S QUANTUM CIRCUIT EMULATOR   |\n" +
-                           " |--------------------------------------------------|\n");
+                " |   WELCOME TO M.ROZE'S QUANTUM CIRCUIT EMULATOR   |\n" +
+                " |--------------------------------------------------|\n");
 
         System.out.println(" help : -h ");
         System.out.println(" exit : -q ");
         Scanner sc = new Scanner(System.in);
-        while (true){
+        HashMap<String, Qubit> qubits = new HashMap<>();
+        while (true) {
             System.out.print("\n >");
             String input = sc.next();
-            switch (input){
-                case "-h" :
+            Parser p = new Parser(input);
+            String firstWord = p.getWord(1);
+
+
+
+            switch (firstWord) {
+                case "-h":
                     System.out.println("\n -- COMMAND LIST -- \n");
-                    System.out.println("create [qubitName] [first coeff] [second coeff] ");
+                    System.out.println("create [qubitName] [first coeff] [second coeff]");
                     System.out.println("entangle [entangledQubitName] [qubitName1] [qubitName2]");
                     System.out.println("collapse [qubitName] [entry]");
                     System.out.println("measure [qubitName] // BEING REWORKED CURRENTLY");
@@ -23,22 +34,48 @@ public class Circuit_Assembler {
                     System.out.println("applyGate [qubitName] [gateCode]");
                     System.out.println("\n -- GATE LIST -- \n");
                     System.out.println("-H (Hadamard)");
-                    System.out.println("-X (Pauli X)" );
-                    System.out.println("-Z (Pauli Z)" );
-                    System.out.println("-Y (Pauli Y)" );
+                    System.out.println("-X (Pauli X)");
+                    System.out.println("-Z (Pauli Z)");
+                    System.out.println("-Y (Pauli Y)");
                     System.out.println("-PS [theta] (Phase shift gate)");
                     System.out.println("-S (Swap)");
                     System.out.println("-Frd (Fredkin)");
                     System.out.println("-Tof (Toffoli)");
                     break;
+                case ("create") :
 
-                
-                case "-q" :
+                    System.out.println("wowow");
+                    break;
+
+
+
+
+                case "-q":
                     System.out.println("\nbyebye !\n");
                     break;
+
             }
 
         }
     }
+    private static class Parser{
 
+        private final String parsed ;
+        Parser(String parsed){
+            this.parsed = parsed;
+        }
+
+        public String getWord(int n){
+            assert parsed.length() != 0 ;
+            assert parsed.contains(" ") : "must contain at least one word" ;
+            String[] words = parsed.split(" ");
+
+            return words[n-1];
+        }
+
+
+
+
+    }
 }
+
